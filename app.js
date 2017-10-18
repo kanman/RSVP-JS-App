1078b4d30e9a7cacb62a35542977634d2f7e5397
“Andrew <andrewgnew@gmail.com>
2017-10-18 23:13:08 +1100

    Added variables and console logging into the app.js file

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+//Assign a variable to the 'registrar' form id - Use a constant as this won't change
+//Assign a variable to the input text field to read the name submitted
+
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+//Add an event listener to the form submission that logs that submission to the console
+//Use e.preventDefault to prevent the browser reloading the page on submission
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();    
+console.log(input.value);
+});
+
+
+
+
