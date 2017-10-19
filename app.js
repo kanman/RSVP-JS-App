60fe4c617fea813a1ea10ad2c4e1ad1f955ba32a
“Andrew <andrewgnew@gmail.com>
2017-10-19 23:07:12 +1100

    Updated index.html

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
 
-//Assign a variable to the 'registrar' form id - Use a constant as this won't change
-//Assign a variable to the input text field to read the name submitted
+//Declaration of main variables
 
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
 
-//Add an event listener to the form submission that logs that submission to the console
-//Use e.preventDefault to prevent the browser reloading the page on submission
+
+//Form Submission function
 
 form.addEventListener('submit', (e) => {
-e.preventDefault();    
-console.log(input.value);
+e.preventDefault();   
+
+const text = input.value;
+
+input.value = '';
+
+
+const li = document.createElement('li');
+li.textContent = text;
+
+
+const label = document.createElement('label');
+label.textContent = ' confirmed';
+
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+label.appendChild(checkbox);
+
+li.appendChild(label);
+
+
+//Adding a button in to each 'li' so we will be able to remove an invitee.
+
+const button = document.createElement('button');
+button.textContent = 'remove';
+li.appendChild(button);
+
+ul.appendChild(li);
 });
+
+//After form submission, we add the change event handler in for the changed checkbox state (check, uncheck)
+
+ul.addEventListener('change', (e) => {
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+
+    //Our variable (listItem) is set to the checkbox, which is a child of the label node, which is a child of the
+    //"li" node.
+    const listItem = checkbox.parentNode.parentNode;
+
+//If statement to activate classname styles if the checkbox is checked.
+    if (checked) {
+    listItem.className = 'responded';    
+    } else {
+    listItem.className = '';    
+    }    
+
+});
+
+
+
 
 
 
